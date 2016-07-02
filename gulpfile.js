44c93c8a65c6fb64616f356c16b8709d42285c29
David Figatner <david@yopeyopey.com>
2016-07-02 14:20:24 +0800

    adding documentation support

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require("gulp"),
-    concat = require('gulp-concat');
-	uglify = require('gulp-uglify');
+    concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+    documentation = require('gulp-documentation');
 
 var files = [
 			'*.js',
             '!gulpfile.js',
-            '!anglejs.min.js'
+            '!*.min.js'
 			];
 var base = {base: '/'};
 
@@ -14,6 +15,12 @@ gulp.task("build", function() {
 		.pipe(concat('anglejs.min.js'))
 	    .pipe(uglify())
 		.pipe(gulp.dest(''));
+});
+
+gulp.task("doc", function() {
+    return gulp.src(files, base)
+        .pipe(documentation({ format: 'html', github: true }))
+        .pipe(gulp.dest('docs'));
 });
 
 gulp.task("default", ['build']);
