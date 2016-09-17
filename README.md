440e7c1ec9490a2e5990293032efb931bad97466
David Figatner <david@yopeyopey.com>
2016-09-17 20:14:57 +0800

    Used class/ES6; getting it ready for NPM publish

--- README.md
+++ README.md
@@ -4,57 +4,191 @@ simple library for calculating angles in javascript
 ## Installation
 include angle.js in your project or add to your workflow
 
-    <script src="update.js"></script>
+    npm install yy-angle
 
-## API Reference
+# API Reference
+**Kind**: global class  
 
-#### Constants (radians)
+* [Angle](#Angle)
+    * [.toDegrees(radians)](#Angle+toDegrees) ⇒ <code>number</code>
+    * [.toRadians(degrees)](#Angle+toRadians) ⇒ <code>number</code>
+    * [.isAngleBetween(target, angle1, angle2)](#Angle+isAngleBetween) ⇒ <code>boolean</code>
+    * [.differenceAnglesSign(target, source)](#Angle+differenceAnglesSign) ⇒ <code>number</code>
+    * [.differenceAngles(a, b)](#Angle+differenceAngles) ⇒ <code>number</code>
+    * [.shortestAngle(start, to)](#Angle+shortestAngle) ⇒ <code>number</code>
+    * [.normalize(radians)](#Angle+normalize) ⇒ <code>number</code>
+    * [.angleTwoPoints([point1], [point2], [x1], [y1], [x2], [y2])](#Angle+angleTwoPoints) ⇒ <code>number</code>
+    * [.distanceTwoPoints([point1], [point2], [x1], [y1], [x2], [y2])](#Angle+distanceTwoPoints) ⇒ <code>number</code>
+    * [.distanceTwoPointsSquared([point1], [point2], [x1], [y1], [x2], [y2])](#Angle+distanceTwoPointsSquared) ⇒ <code>number</code>
+    * [.closestAngle(angle)](#Angle+closestAngle) ⇒ <code>number</code>
+    * [.equals(a1, a2, a1)](#Angle+equals)
 
-* Angle.UP
-* Angle.DOWN
-* Angle.LEFT
-* Angle.RIGHT
+<a name="Angle+toDegrees"></a>
 
-* Angle.NORTH
-* Angle.SOUTH
-* Angle.EAST
-* Angle.WEST
+### angle.toDegrees(radians) ⇒ <code>number</code>
+converts from radians to degrees (all other functions expect radians)
 
-* Angle.PI_2 (PI x 2)
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - degrees  
 
-#### Angle.angleTwoPoints(x1, y1, x2, y2)
-finds angle between two points (radians)
-* alternatively accepts (p1, p2) where p are point objects {x: x, y: y}
+| Param | Type |
+| --- | --- |
+| radians | <code>number</code> | 
 
-#### Angle.differenceAngles(a, b)
-determines the normalized difference between two angles (radians)
+<a name="Angle+toRadians"></a>
 
-#### Angle.differenceAnglesSign(target, source)
-determines whether the difference between twoangles is positive or negative (radians)
+### angle.toRadians(degrees) ⇒ <code>number</code>
+converts from degrees to radians (all other functions expect radians)
 
-#### Angle.distanceTwoPoints(x1, y1, x2, y2)
-finds distance between two points
-* alternatively accepts (p1, p2) where p are point objects {x: x, y: y}
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - radians  
 
-Angle.distanceTwoPointsSquared(x1, y1, x2, y2)
-finds the square distance between two points
-* alternatively accepts (p1, p2) where p are point objects {x: x, y: y}
+| Param | Type |
+| --- | --- |
+| degrees | <code>number</code> | 
 
-#### Angle.isAngleBetween(target, angle1, angle2)
-Checks whether the target is between angle1 and angle2 (radians)
+<a name="Angle+isAngleBetween"></a>
 
-#### Angle.normalize(radians, radian)
-normalizes angle [0 - PI x 2) (radians)
+### angle.isAngleBetween(target, angle1, angle2) ⇒ <code>boolean</code>
+returns whether the target angle is between angle1 and angle2 (in radians)
+(based on: http://stackoverflow.com/questions/11406189/determine-if-angle-lies-between-2-other-angles)
 
-#### Angle.toDegrees(radian, radians)
-radian to degrees
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
 
-#### Angle.toRadian(degrees)
-degrees to Radian
+| Param | Type | Description |
+| --- | --- | --- |
+| target | <code>number</code> | angle |
+| angle1 | <code>number</code> |  |
+| angle2 | <code>number</code> |  |
 
-#### Angle.closestAngle(angle)
-finds the closest cardinal (N, S, E, W) to the given angle
-* returns the radian angle
+<a name="Angle+differenceAnglesSign"></a>
 
-## License
-MIT License (MIT)
+### angle.differenceAnglesSign(target, source) ⇒ <code>number</code>
+returns +1 or -1 based on whether the difference between two angles is positive or negative (in radians)
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - 1 or -1  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| target | <code>number</code> | angle |
+| source | <code>number</code> | angle |
+
+<a name="Angle+differenceAngles"></a>
+
+### angle.differenceAngles(a, b) ⇒ <code>number</code>
+returns the normalized difference between two angles (in radians)
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - normalized difference between a and b  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| a | <code>number</code> | first angle |
+| b | <code>number</code> | second angle |
+
+<a name="Angle+shortestAngle"></a>
+
+### angle.shortestAngle(start, to) ⇒ <code>number</code>
+returns a target angle that is the shortest way to rotate an object between start and to--may choose a negative angle
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - shortest target angle  
+
+| Param | Type |
+| --- | --- |
+| start | <code>number</code> | 
+| to | <code>number</code> | 
+
+<a name="Angle+normalize"></a>
+
+### angle.normalize(radians) ⇒ <code>number</code>
+returns the normalized angle (0 - PI x 2)
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - normalized angle in radians  
+
+| Param | Type |
+| --- | --- |
+| radians | <code>number</code> | 
+
+<a name="Angle+angleTwoPoints"></a>
+
+### angle.angleTwoPoints([point1], [point2], [x1], [y1], [x2], [y2]) ⇒ <code>number</code>
+returns angle between two points (in radians)
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - angle  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| [point1] | <code>Point</code> | {x: x, y: y} |
+| [point2] | <code>Point</code> | {x: x, y: y} |
+| [x1] | <code>number</code> |  |
+| [y1] | <code>number</code> |  |
+| [x2] | <code>number</code> |  |
+| [y2] | <code>number</code> |  |
+
+<a name="Angle+distanceTwoPoints"></a>
+
+### angle.distanceTwoPoints([point1], [point2], [x1], [y1], [x2], [y2]) ⇒ <code>number</code>
+returns distance between two points
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - distance  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| [point1] | <code>Point</code> | {x: x, y: y} |
+| [point2] | <code>Point</code> | {x: x, y: y} |
+| [x1] | <code>number</code> |  |
+| [y1] | <code>number</code> |  |
+| [x2] | <code>number</code> |  |
+| [y2] | <code>number</code> |  |
+
+<a name="Angle+distanceTwoPointsSquared"></a>
+
+### angle.distanceTwoPointsSquared([point1], [point2], [x1], [y1], [x2], [y2]) ⇒ <code>number</code>
+returns the squared distance between two points
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - squared distance  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| [point1] | <code>Point</code> | {x: x, y: y} |
+| [point2] | <code>Point</code> | {x: x, y: y} |
+| [x1] | <code>number</code> |  |
+| [y1] | <code>number</code> |  |
+| [x2] | <code>number</code> |  |
+| [y2] | <code>number</code> |  |
+
+<a name="Angle+closestAngle"></a>
+
+### angle.closestAngle(angle) ⇒ <code>number</code>
+returns the closest cardinal (N, S, E, W) to the given angle (in radians)
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+**Returns**: <code>number</code> - closest cardinal in radians  
+
+| Param | Type |
+| --- | --- |
+| angle | <code>number</code> | 
+
+<a name="Angle+equals"></a>
+
+### angle.equals(a1, a2, a1)
+checks whether angles a1 and a2 are equal (after normalizing)
+
+**Kind**: instance method of <code>[Angle](#Angle)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| a1 | <code>number</code> |  |
+| a2 | <code>number</code> |  |
+| a1 | <code>boolean</code> | === a2 |
+
+
+* * *
+
+Copyright (c) 2016 YOPEY YOPEY LLC - MIT License - Documented by [jsdoc-to-markdown](https://github.com/75lb/jsdoc-to-markdown)
--- angle.js
+++ angle.js
@@ -1,225 +1,240 @@
 /*
     angle.js <https://github.com/davidfig/anglejs>
-    Released under MIT license <https://github.com/davidfig/anglejs/license>
-    Author David Figatner and other contributors
-    Copyright YOPEY YOPEY LLC and other contributors
+    Released under MIT license <https://github.com/davidfig/angle/blob/master/LICENSE>
+    Author: David Figatner
+    Copyright (c) 2016 YOPEY YOPEY LLC
 */
 
-// constants
-var UP = Math.PI / 2;
-var DOWN = 3 * Math.PI / 2;
-var LEFT = Math.PI;
-var RIGHT = 0;
-
-var NORTH = UP;
-var SOUTH = DOWN;
-var WEST = LEFT;
-var EAST = RIGHT;
-
-var PI_2 = Math.PI * 2;
-var PI_QUARTER = Math.PI / 4;
-var PI_HALF = Math.PI / 2;
-
-// converts between degrees and radians; all other functions expect radians
-
-var _toDegreeConversion = 180 / Math.PI;
-function toDegrees(radians)
+/** @class */
+class Angle
 {
-    return radians * _toDegreeConversion;
-}
-
-var _toRadianConversion = Math.PI / 180;
-function toRadians(degrees)
-{
-    return radians * _toRadianConversion;
-}
-
-// returns whether the target angle is between angle1 and angle2 (in radians)
-// (based on: http://stackoverflow.com/questions/11406189/determine-if-angle-lies-between-2-other-angles)
-function isAngleBetween(target, angle1, angle2)
-{
-    var rAngle = ((angle2 - angle1) % PI_2 + PI_2) % PI_2;
-    if (rAngle >= Math.PI)
+    constructor()
     {
-        var swap = angle1;
-        angle1 = angle2;
-        angle2 = swap;
+        // constants
+        this.UP = Math.PI / 2;
+        this.DOWN = 3 * Math.PI / 2;
+        this.LEFT = Math.PI;
+        this.RIGHT = 0;
+
+        this.NORTH = this.UP;
+        this.SOUTH = this.DOWN;
+        this.WEST = this.LEFT;
+        this.EAST = this.RIGHT;
+
+        this.PI_2 = Math.PI * 2;
+        this.PI_QUARTER = Math.PI / 4;
+        this.PI_HALF = Math.PI / 2;
+
+        this._toDegreeConversion = 180 / Math.PI;
+        this._toRadianConversion = Math.PI / 180;
     }
 
-    if (angle1 <= angle2)
+    /**
+     * converts from radians to degrees (all other functions expect radians)
+     * @param {number} radians
+     * @return {number} degrees
+     */
+    toDegrees(radians)
     {
-        return target >= angle1 && target <= angle2;
+        return radians * this._toDegreeConversion;
     }
-    else
+
+    /**
+     * converts from degrees to radians (all other functions expect radians)
+     * @param {number} degrees
+     * @return {number} radians
+     */
+    toRadians(degrees)
     {
-        return target >= angle1 || target <= angle2;
+        return degrees * this._toRadianConversion;
+    }
+
+    /**
+     * returns whether the target angle is between angle1 and angle2 (in radians)
+     * (based on: http://stackoverflow.com/questions/11406189/determine-if-angle-lies-between-2-other-angles)
+     * @param {number} target angle
+     * @param {number} angle1
+     * @param {number} angle2
+     * @return {boolean}
+     */
+    isAngleBetween(target, angle1, angle2)
+    {
+        const rAngle = ((angle2 - angle1) % this.PI_2 + this.PI_2) % this.PI_2;
+        if (rAngle >= Math.PI)
+        {
+            const swap = angle1;
+            angle1 = angle2;
+            angle2 = swap;
+        }
+
+        if (angle1 <= angle2)
+        {
+            return target >= angle1 && target <= angle2;
+        }
+        else
+        {
+            return target >= angle1 || target <= angle2;
+        }
+    }
+
+    /**
+     * returns +1 or -1 based on whether the difference between two angles is positive or negative (in radians)
+     * @param {number} target angle
+     * @param {number} source angle
+     * @return {number} 1 or -1
+     */
+    differenceAnglesSign(target, source)
+    {
+        function mod(a, n)
+        {
+            return (a % n + n) % n;
+        }
+
+        const a = target - source;
+        return mod((a + Math.PI), this.PI_2) - Math.PI > 0 ? 1 : -1;
+    }
+
+    /**
+     * returns the normalized difference between two angles (in radians)
+     * @param {number} a - first angle
+     * @param {number} b - second angle
+     * @return {number} normalized difference between a and b
+     */
+    differenceAngles(a, b)
+    {
+        const c = Math.abs(a - b) % this.PI_2;
+        return c > Math.PI ? (this.PI_2 - c) : c;
+    }
+
+    /**
+     * returns a target angle that is the shortest way to rotate an object between start and to--may choose a negative angle
+     * @param {number} start
+     * @param {number} to
+     * @return {number} shortest target angle
+     */
+    shortestAngle(start, to)
+    {
+        const difference = this.differenceAngles(to, start);
+        const sign = this.differenceAnglesSign(to, start);
+        const delta = difference * sign;
+        return delta + start;
+    }
+
+    /**
+     * returns the normalized angle (0 - PI x 2)
+     * @param {number} radians
+     * @return {number} normalized angle in radians
+     */
+    normalize(radians)
+    {
+        return radians - this.PI_2 * Math.floor(radians / this.PI_2);
+    }
+
+    /**
+     * returns angle between two points (in radians)
+     * @param {Point} [point1] {x: x, y: y}
+     * @param {Point} [point2] {x: x, y: y}
+     * @param {number} [x1]
+     * @param {number} [y1]
+     * @param {number} [x2]
+     * @param {number} [y2]
+     * @return {number} angle
+     */
+    angleTwoPoints(/* (point1, point2) OR (x1, y1, x2, y2) */)
+    {
+        if (arguments.length === 4)
+        {
+            return Math.atan2(arguments[3] - arguments[1], arguments[2] - arguments[0]);
+        }
+        else
+        {
+            return Math.atan2(arguments[1].y - arguments[0].y, arguments[1].x - arguments[0].x);
+        }
+    }
+
+    /**
+     * returns distance between two points
+     * @param {Point} [point1] {x: x, y: y}
+     * @param {Point} [point2] {x: x, y: y}
+     * @param {number} [x1]
+     * @param {number} [y1]
+     * @param {number} [x2]
+     * @param {number} [y2]
+     * @return {number} distance
+     */
+    distanceTwoPoints(/* (point1, point2) OR (x1, y1, x2, y2) */)
+    {
+        if (arguments.length === 2)
+        {
+            return Math.sqrt(Math.pow(arguments[1].x - arguments[0].x, 2) + Math.pow(arguments[1].y - arguments[0].y, 2));
+        }
+        else
+        {
+            return Math.sqrt(Math.pow(arguments[2] - arguments[0], 2) + Math.pow(arguments[3] - arguments[1], 2));
+        }
+    }
+
+    /**
+     * returns the squared distance between two points
+     * @param {Point} [point1] {x: x, y: y}
+     * @param {Point} [point2] {x: x, y: y}
+     * @param {number} [x1]
+     * @param {number} [y1]
+     * @param {number} [x2]
+     * @param {number} [y2]
+     * @return {number} squared distance
+     */
+    distanceTwoPointsSquared(/* (point1, point2) OR (x1, y1, x2, y2) */)
+    {
+        if (arguments.length === 2)
+        {
+            return Math.pow(arguments[1].x - arguments[0].x, 2) + Math.pow(arguments[1].y - arguments[0].y, 2);
+        }
+        else
+        {
+            return Math.pow(arguments[2] - arguments[0], 2) + Math.pow(arguments[3] - arguments[1], 2);
+        }
+    }
+
+    /**
+     * returns the closest cardinal (N, S, E, W) to the given angle (in radians)
+     * @param {number} angle
+     * @return {number} closest cardinal in radians
+     */
+    closestAngle(angle)
+    {
+        var left = this.differenceAngles(angle, this.LEFT);
+        var right = this.differenceAngles(angle, this.RIGHT);
+        var up = this.differenceAngles(angle, this.UP);
+        var down = this.differenceAngles(angle, this.DOWN);
+        if (left <= right && left <= up && left <= down)
+        {
+            return this.LEFT;
+        }
+        else if (right <= up && right <= down)
+        {
+            return this.RIGHT;
+        }
+        else if (up <= down)
+        {
+            return this.UP;
+        }
+        else
+        {
+            return this.DOWN;
+        }
+    }
+
+    /**
+     * checks whether angles a1 and a2 are equal (after normalizing)
+     * @param {number} a1
+     * @param {number} a2
+     * @param {boolean} a1 === a2
+     *  */
+    equals(a1, a2)
+    {
+        return this.normalize(a1) === this.normalize(a2);
     }
 }
 
-// returns +1 or -1 based on whether the difference between two angles is positive or negative (in radians)
-// EXAMPLE: var actualAngleDelta = differenceAngles(target, source) * differenceAnglesSign(target, source);
-function differenceAnglesSign(target, source)
-{
-    function mod(a, n)
-    {
-        return (a % n + n) % n;
-    }
-
-    var a = target - source;
-    return mod((a + Math.PI), PI_2) - Math.PI > 0 ? 1 : -1;
-}
-
-// returns the normalized difference between two angles (in radians)
-function differenceAngles(a, b)
-{
-    var c = Math.abs(a - b) % PI_2;
-    return c > Math.PI ? (PI_2 - c) : c;
-}
-
-// returns a target angle that is the shortest way to rotate an object between start and to (i.e., may choose a negative angle)
-function shortestAngle(start, to)
-{
-    var difference = differenceAngles(to, start);
-    var sign = differenceAnglesSign(to, start);
-    var delta = difference * sign;
-    return delta + start;
-}
-
-// returns the normalized angle (0 - PI x 2) (in radians)
-function normalize(radians)
-{
-    return radians - PI_2 * Math.floor(radians / PI_2);
-}
-
-/**
- * finds angle between two points (radians)
- * also accepts 2 point objects {x: x, y: y}
- * @param  {number} x1
- * @param  {number} y1
- * @param  {number} x2
- * @param  {number} y2
- * @return {number} radian
- */
-
-// returns angle between two points (in radians)
-// PARAMS: (x1, y1, x2, y2) or (point1, point2) where point = {x: x, y: y}
-function angleTwoPoints()
-{
-    if (arguments.length === 4)
-    {
-        return Math.atan2(arguments[3] - arguments[1], arguments[2] - arguments[0]);
-    }
-    else
-    {
-        return Math.atan2(arguments[1].y - arguments[0].y, arguments[1].x - arguments[0].x);
-    }
-}
-
-// returns distance between two points
-// PARAMS: (x1, y1, x2, y2) or (point1, point2) where point = {x: x, y: y}
-function distanceTwoPoints()
-{
-    if (arguments.length === 2)
-    {
-        return Math.sqrt(Math.pow(arguments[1].x - arguments[0].x, 2) + Math.pow(arguments[1].y - arguments[0].y, 2));
-    }
-    else
-    {
-        return Math.sqrt(Math.pow(arguments[2] - arguments[0], 2) + Math.pow(arguments[3] - arguments[1], 2));
-    }
-}
-
-// returns the squared distance between two points
-// PARAMS: (x1, y1, x2, y2) or (point1, point2) where point = {x: x, y: y}
-function distanceTwoPointsSquared()
-{
-    if (arguments.length === 2)
-    {
-        return Math.pow(arguments[1].x - arguments[0].x, 2) + Math.pow(arguments[1].y - arguments[0].y, 2);
-    }
-    else
-    {
-        return Math.pow(arguments[2] - arguments[0], 2) + Math.pow(arguments[3] - arguments[1], 2);
-    }
-}
-
-// returns the closest cardinal (N, S, E, W) to the given angle (in radians)
-function closestAngle(angle)
-{
-    var left = differenceAngles(angle, LEFT);
-    var right = differenceAngles(angle, RIGHT);
-    var up = differenceAngles(angle, UP);
-    var down = differenceAngles(angle, DOWN);
-    if (left <= right && left <= up && left <= down)
-    {
-        return LEFT;
-    }
-    else if (right <= up && right <= down)
-    {
-        return RIGHT;
-    }
-    else if (up <= down)
-    {
-        return UP;
-    }
-    else
-    {
-        return DOWN;
-    }
-}
-
-// returns whether a1 is equal to a2
-function equals(a1, a2)
-{
-    return normalize(a1) === normalize(a2);
-}
-
-// exports
-var Angle = {
-    UP: UP,
-    DOWN: DOWN,
-    LEFT: LEFT,
-    RIGHT: RIGHT,
-    NORTH: NORTH,
-    SOUTH: SOUTH,
-    EAST: EAST,
-    WEST: WEST,
-    PI_2: PI_2,
-    PI_HALF: PI_HALF,
-    PI_QUARTER: PI_QUARTER,
-    toDegrees: toDegrees,
-    toRadians: toRadians,
-    isAngleBetween: isAngleBetween,
-    differenceAnglesSign: differenceAnglesSign,
-    differenceAngles: differenceAngles,
-    shortestAngle: shortestAngle,
-    normalize: normalize,
-    angleTwoPoints: angleTwoPoints,
-    distanceTwoPoints: distanceTwoPoints,
-    distanceTwoPointsSquared: distanceTwoPointsSquared,
-    closestAngle: closestAngle,
-    equals: equals
-};
-
-// add support for AMD (Asynchronous Module Definition) libraries such as require.js.
-if (typeof define === 'function' && define.amd)
-{
-    define(function()
-    {
-        return {
-            Angle: Angle
-        };
-    });
-}
-
-// add support for CommonJS libraries such as browserify.
-if (typeof exports !== 'undefined')
-{
-    module.exports = Angle;
-}
-
-// define globally in case AMD is not available or available but not used
-if (typeof window !== 'undefined')
-{
-    window.Angle = Angle;
-}
+module.exports = new Angle();
